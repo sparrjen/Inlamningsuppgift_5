84f0d006d31b574fd771f59bad2a627baafb4646
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-25 13:55:41 +0200

    Add project files.

---
+++ DeviceApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceApp
+{
+    class Program
+    {
+
+        private static DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=ecwin20-iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=2ckDw25bKM0kQaM7e9ohZ+T8WKhVoUz/+AMkGTEHY5c=", TransportType.Mqtt);
+        private static int telemetryInterval = 5; 
+        
+        
+        static void Main(string[] args)
+        {
+        
+        }
+
+        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext)
+        {
+            var payload = Encoding.UTF8.GetString(request.Data); 
+
+            if(Int32.TryParse(payload, out telemetryInterval))
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+    }
+}

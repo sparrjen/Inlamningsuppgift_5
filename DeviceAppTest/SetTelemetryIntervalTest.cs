fd25a0e4fb2bbc7833056921eb0552eb1d5a3e0d
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-29 10:16:25 +0200

    Inlamningsuppgift_4

--- DeviceAppTest/SetTelemetryIntervalTest.cs
+++ DeviceAppTest/SetTelemetryIntervalTest.cs
@@ -1,20 +1,22 @@
 using System;
 using Xunit;
+using DeviceApp.Services;
+
 using Microsoft.Azure.Devices.Client;
 
 namespace DeviceAppTest
 {
     public class SetTelemetryIntervalTests
     {
-        private DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=ecwin20-iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=2ckDw25bKM0kQaM7e9ohZ+T8WKhVoUz/+AMkGTEHY5c=", TransportType.Mqtt);
+        public DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=ecwin20-iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=2ckDw25bKM0kQaM7e9ohZ+T8WKhVoUz/+AMkGTEHY5c=", TransportType.Mqtt);
 
         [Theory]   
         [InlineData("SetTelemetryInterval", "10", 200)]
         [InlineData("SetInterval", "10", 501)]
         public void SetTelemetryInterval_ShouldChangeTelemetryInterval(string methodName, string payload, int statusCode)
         {
-            deviceClient.SetMethodHandlerAsync(methodName, SetTelemetryInterval, null).Wait();
-           // Assert.Equal?
+            deviceClient.SetMethodHandlerAsync(methodName, DeviceServices.SetTelemetryInterval, null).Wait();
+           
         }
     }
 }
---
+++ ServiceApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=ecwin20-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=lwUuS95hv+CttAQvkrF7s9kuNMXLm0Wd4WYcM5YIG7M=");
+
+
+        static void Main(string[] args)
+        {
+            Task.Delay(5000).Wait();
+
+            InvokeMethod("DeviceApp", "SetTelemetryInterval", "10").GetAwaiter();
+            Console.ReadKey();
+        }
+        static async Task InvokeMethod(string deviceId, string methodName)
+        {
+            var methodInvocation = new CloudToDeviceMethod(methodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson(payload);
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, methodInvocation);
+            Console.WriteLine($"Response Status: {response.Status}");
+            Console.WriteLine(response.GetPayloadAsJson());
+        }
+
+    }
+}
